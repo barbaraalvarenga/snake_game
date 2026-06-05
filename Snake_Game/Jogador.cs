6ce41596c15a8e2ae0871feb473f556273eff7bf
Barbarac-dev <156028148+barbaraalvarenga@users.noreply.github.com>
2026-06-05 10:24:38 -0300

    Add Classe Jogador

---
+++ Snake_Game/Jogador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Jogador
+{
+
+    public string Nome;
+
+    private int pontuacao;
+
+
+    public Jogador(string nome)
+
+    {
+
+        Nome = nome;
+
+        pontuacao = 0;
+
+    }
+
+
+    public int Pontuacao
+
+    {
+
+        get { return pontuacao; }
+
+        set { pontuacao = value; }
+
+    }
+
+
+    public void AdicionarPontos()
+
+    {
+
+        pontuacao += 10;
+
+    }
+
+
+    public void RegistrarPontuacao()
+
+    {
+
+        using (StreamWriter sw = new StreamWriter("scores.txt", true))
+
+        {
+
+            sw.WriteLine(Nome + ";" + pontuacao);
+
+        }
+
+    }
+
+}
